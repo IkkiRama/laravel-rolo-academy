dd8fb9bcbe0bd6c1384bb09375f0a3c9466fdfd2
IkkiRama <georgeikkirama@gmail.com>
2021-05-09 16:45:53 +0700

    Vidio ke 2 Database Dan Migration

---
+++ database/migrations/2021_05_09_092041_create_siswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_depan', 100);
+            $table->string('nama_belakang', 100);
+            $table->string('jenis_kelamin', 100);
+            $table->string('agama', 100);
+            $table->text('alamat', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}

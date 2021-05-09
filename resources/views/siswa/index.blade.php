d965723630030a2557898fbcf5bebdf1b74411d1
IkkiRama <georgeikkirama@gmail.com>
2021-05-09 18:00:40 +0700

    Vidio ke 6 Bootstrap

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,21 +1,99 @@
-<h2>Data Siswa</h2>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<table border="1" cellpadding="10" cellspacing="0">
-    <tr>
-        <th>No</th>
-        <th>Nama</th>
-        <th>Jenis Kelamin</th>
-        <th>Agama</th>
-        <th>Alamat</th>
-    </tr>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    @foreach($siswa as $value)
-    <tr>
-        <td>{{$loop->iteration}}</td>
-        <td>{{$value->nama_depan}} {{$value->nama_belakang}}</td>
-        <td>{{$value->jenis_kelamin}}</td>
-        <td>{{$value->agama}}</td>
-        <td>{{$value->alamat}}</td>
-    </tr>
-    @endforeach
-</table>
+    <title>Daftar Siswa</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <h2>Data Siswa</h2>
+
+            <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">Tambah Data</button>
+
+        <table class="table table-bordered table-hover table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Agama</th>
+                    <th>Alamat</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach($siswa as $value)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$value->nama_depan}} {{$value->nama_belakang}}</td>
+                    <td>{{$value->jenis_kelamin}}</td>
+                    <td>{{$value->agama}}</td>
+                    <td>{{$value->alamat}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-dialog-scrollable  modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Form Tambah Data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="nama_depan">Nama Depan</label>
+                            <input type="text" name="nama_depan" id="nama_depan" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="nama_belakang">Nama Belakang</label>
+                            <input type="text" name="nama_belakang" id="nama_belakang" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="jenis_kelamin">Jenis Kelamin</label>
+                            <select name="jenis_kelamin" id="jenis_kelamin" class="form-control">
+                                <option value="">Pilih Jenis Kelamin</option>
+                                <option value="laki-laki">Laki-laki</option>
+                                <option value="perempuan">Perempuan</option>
+                            </select>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="agama">Agama</label>
+                            <input type="text" name="agama" id="agama" class="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="alamat">Alamat</label>
+                            <input type="text" name="alamat" id="alamat" class="form-control">
+                        </div>
+
+
+                        <button class="btn btn-primary">Tambah Data</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  </body>
+</html>

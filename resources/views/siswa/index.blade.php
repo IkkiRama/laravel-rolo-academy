4b3f7b5156ce21be69aebadcf375ea8b4fefbe92
IkkiRama <georgeikkirama@gmail.com>
2021-05-09 17:17:57 +0700

    Vidio ke 5 View

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,1 +1,21 @@
-{{dd($siswa)}}
+<h2>Data Siswa</h2>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>Jenis Kelamin</th>
+        <th>Agama</th>
+        <th>Alamat</th>
+    </tr>
+
+    @foreach($siswa as $value)
+    <tr>
+        <td>{{$loop->iteration}}</td>
+        <td>{{$value->nama_depan}} {{$value->nama_belakang}}</td>
+        <td>{{$value->jenis_kelamin}}</td>
+        <td>{{$value->agama}}</td>
+        <td>{{$value->alamat}}</td>
+    </tr>
+    @endforeach
+</table>

401415526e5df6d272f412972806c020dc2aca71
IkkiRama <georgeikkirama@gmail.com>
2021-05-11 10:38:05 +0700

    Vidio 16 Relation Mata Pelajaran

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -10,4 +10,9 @@ class Siswa extends Model
     use HasFactory;
     protected $table = 'siswa';
     protected $fillable = ['nama_depan', 'nama_belakang', 'agama', 'jenis_kelamin', 'alamat', 'foto', 'id_user'];
+
+    public function mapel()
+    {
+        return $this->belongsToMany(Mapel::class)->withPivot(['nilai']);
+    }
 }
